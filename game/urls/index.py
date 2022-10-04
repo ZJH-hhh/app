30872f300df7d92f01ca781b1c7ed0efc2c32e50
zjh <zjh001122@outlook.com>
2022-10-04 20:25:51 +0800

    solve some problems

--- game/urls/index.py
+++ game/urls/index.py
@@ -1,8 +1,8 @@
 from django.urls import path, include
-from django.views.index import index
+from game.views.index import index
 
 urlpatterns = [
-    path("", index, name="global_index")
+    path("", index, name="global_index"),
     path("menu/", include("game.urls.menu.index")),
     path("playground/", include("game.urls.playground.index")),
     path("settings/", include("game.urls.settings.index")),
