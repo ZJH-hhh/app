6147fadfc462b531c3c1ff04007928481b011c08
zjh <zjh001122@outlook.com>
2022-10-14 20:02:52 +0800

    write playground more

---
+++ game/static/js/src/playground/ac_game_object/zbase.js
@@ -0,0 +1,48 @@
+let AC_GAME_OBJECTS = [];
+
+class AcGameObject {
+    constructor() {
+        AC_GAME_OBJECTS.push(this);
+
+        this.has_called_start = false; // 当前是否执行过start函数，因为start只需执行一次，所以要特判一下
+        this.timedelta = 0; // 当前帧距离上一帧的时间间隔
+    }
+
+    start() { // 只在第一帧执行一次
+    }
+
+    update() { // 每一帧都会执行一次
+    }
+
+    before_destroy() { // 删掉物体前执行一次
+    }
+
+    destroy() { // 删掉该物体
+        this.before_destroy();
+
+        for (let i = 0; i < AC_GAME_OBJECTS.length; i ++ ) {
+            if (AC_GAME_OBJECTS[i] === this) { // 三个等号，js中加了一个判断类型相等的等号
+                AC_GAME_OBJECTS.splice(i, 1); // js删除，从位置i删除1个
+                break;
+            }
+        }
+    }
+}
+
+let last_timestamp;
+let AC_GAME_ANIMATION = function(timestamp) {
+    for (let i = 0; i < AC_GAME_OBJECTS.length; i ++ ) {
+        let obj = AC_GAME_OBJECTS[i];
+        if (!obj.has_called_start) {
+            obj.start();
+            obj.has_called_start = true;
+        } else {
+            obj.timedelta = timestamp - last_timestamp;
+            obj.update();
+        }
+    }
+    last_timestamp = timestamp;
+    requestAnimationFrame(AC_GAME_ANIMATION);
+}
+
+requestAnimationFrame(AC_GAME_ANIMATION);
